e7e00f86331d8d977b2c013f16db893cb68bb694
shlomif <shlomif@50811bd7-b8ce-0310-adc1-d9db26280581>
2010-03-19 06:03:11 +0000

    Add the multiple 'category' note.
    git-svn-id: https://svn.perl.org/modules/XML-RSS/trunk@13871 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/XML/RSS.pm
+++ lib/XML/RSS.pm
@@ -1731,8 +1731,17 @@ The default B<mode>
 is append, which adds the item to the end of the list. To insert an item, set the mode
 to B<insert>.
 
-The items are stored in the array @{$obj->{'items'}} where
+The items are stored in the array C<< @{$obj->{'items'}} >> where
 B<$obj> is a reference to an XML::RSS object.
+
+One can specify a category by using the B<'category'> key. B<'category'> can
+point to an array reference of categories:
+
+    $rss->add_item(
+        title => "Foo&Bar",
+        link => "http://www.my.tld/",
+        category => ["OneCat", "TooCat", "3Kitties"],
+    );
 
 =item as_string;
 
